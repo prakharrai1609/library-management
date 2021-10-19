4ed231941a4cadf6b8e56021c362a8bdf61215a5
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2021-10-19 19:38:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const name = document.getElementById('name');
 const author = document.getElementById('author');
-const type = document.querySelectorAll('#type');
+const type = document.querySelectorAll('#radio-box');
 const table = document.getElementById('table-body');
 const button = document.getElementById('addbook');
 const library = document.getElementById('library-form');
@@ -17,7 +17,7 @@ class Library {
         this.author = author;
         type.forEach((e) => {
             if (e.checked) {
-                this.type = e.name;
+                this.type = e.value;
             }
         })
     }
@@ -52,7 +52,19 @@ class Library {
     }
 
     validate = () => {
-        if ((this.name).length < 3 || (this.author).length < 3 || (this.type.checked)) {
+        if ((this.name).length < 3 || (this.author).length < 3) {
+            this.warn();
+            return false;
+        }
+
+        let flag = 1;
+        type.forEach((e) => {
+            if (e.checked) {
+                flag = 0;
+            }
+        })
+
+        if (flag) {
             this.warn();
             return false;
         } else {
@@ -75,6 +87,9 @@ class Library {
     }
 }
 
+function deleteBook(book) {
+
+}
 
 button.addEventListener('click', function () {
     book = new Library(name.value, author.value, type);
@@ -83,9 +98,9 @@ button.addEventListener('click', function () {
         show();
 
         const del = document.querySelectorAll('#del');
-        del.forEach((e, index) => {
-            e.addEventListener('click', () => {
-                del[index].remove();
+        del.forEach((e) => {
+            e.addEventListener('click', (element, index) => {
+                e.remove();
                 let store = localStorage.getItem('store');
                 let temp = JSON.parse(store);
                 temp.splice(index, 1);
@@ -121,29 +136,28 @@ let show = () => {
 
 // view all previous books after reloading the session
 let temp = localStorage.getItem('store');
-let refresh = 0;
-if (temp != null && !refresh) {
+if (temp != null) {
     let store = localStorage.getItem('store');
     let books = JSON.parse(store);
     let html = '';
+
     books.forEach((e) => {
         html += `
             <tr id="del">
                 <td>${e[0]}</td>
                 <td>${e[1]}</td>
                 <td>${e[2]}</td>
-                <td><button style="padding: 0rem 0.5rem;" class="btn btn-danger">Delete</button></td>
+                <td><button style="padding: 0rem 0.5rem;" class="btn btn-danger" id="del">Delete</button></td>
             </tr>
             `
 
     });
-    table.innerHTML += html;
-    library.reset();
-    refresh = 1;
+
+    table.innerHTML = html;
     const del = document.querySelectorAll('#del');
-    del.forEach((e, index) => {
-        e.addEventListener('click', () => {
-            del[index].remove();
+    del.forEach((e) => {
+        e.addEventListener('click', (elem, index) => {
+            e.remove();
             let store = localStorage.getItem('store');
             let temp = JSON.parse(store);
             temp.splice(index, 1);
